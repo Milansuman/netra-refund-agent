366f7b5d82f85c99ee02521bdd6485c577d2d371
Milansuman <milansuman2016@gmail.com>
2026-02-10 12:33:21 +0530

    add script logs

--- run-simulation-1.ps1
+++ run-simulation-1.ps1
@@ -1,1 +1,2 @@
+echo "Running the failed simulation"
 Invoke-RestMethod -Method Post -Uri "http://localhost:8000/run-simulation/312523fc-9810-4621-aebd-796ffece13e0?failed=true"
--- run-simulation-2.ps1
+++ run-simulation-2.ps1
@@ -1,1 +1,2 @@
+echo "Running the simulation"
 Invoke-RestMethod -Method Post -Uri "http://localhost:8000/run-simulation/312523fc-9810-4621-aebd-796ffece13e0"
